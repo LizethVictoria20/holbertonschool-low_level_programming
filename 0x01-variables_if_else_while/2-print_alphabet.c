95eb33c99d50cf3cba377be64958dabd7cd73d86
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-06 19:46:40 +0000

    Print alphabet lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Printing alphabet in lowercase
+ * Return: 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+	putchar('\n');
+	return (0);
+}
