c5a907328d590022917aba44e1e7340b040b01e5
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-11 16:12:39 +0000

    Is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+#include "holberton.h"
+/**
+ * _islower - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+	if (_islower(c))
+		return (c + '1');
+	else
+		return (c + '0');
+}
