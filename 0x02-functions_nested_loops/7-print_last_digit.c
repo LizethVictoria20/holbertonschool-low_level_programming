37fa99a8e5697f8fc26cae0df7dc8c43f33dbadf
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-11 21:41:49 +0000

    Last digital

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int print_last_digit(int n)
+{
+	int last;
+	int m;
+	last = n % 10;
+	if(last < 0)
+	{
+		m = last * -1;
+		_putchar(m + '0');
+		return (m);
+	}
+	else
+	{
+		_putchar(last + '0');
+		return (last);
+	}
+}
