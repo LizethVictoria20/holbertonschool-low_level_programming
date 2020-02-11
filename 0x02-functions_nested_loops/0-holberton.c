589649e2552494d398025ac15b3c716e2d5dc746
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-11 14:59:32 +0000

    Update

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -2,6 +2,14 @@
 
 int main(void)
 {
-	_putchar('Holberton');
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	return (0);
 }
