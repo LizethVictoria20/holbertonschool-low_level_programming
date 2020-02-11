301fa6a62c7a840fe8a82743ec0cd9b6c397049f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-11 22:06:46 +0000

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * add - check the code for Holberton School students.
+ *@a: Variables one
+ *@b: Variable two
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	int add = a + b;
+	return (add);
+}
