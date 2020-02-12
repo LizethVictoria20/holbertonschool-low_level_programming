85c0e60fbe3a609c41898c7e9db516a0e2815cb1
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-12 01:01:37 +0000

    Matural numer

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Program to sum integers
- * Print natural number
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-
 int main(void)
 {
 	int a, b;
