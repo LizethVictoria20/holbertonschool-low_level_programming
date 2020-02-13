684203026ea0585e5f83126385045021da5e6a6c
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-13 18:16:58 +0000

    Multiplies two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - check the code for Holberton School students.
+ * @a: Variable
+ * @b: Variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	int multi = a * b;
+	return (multi);
+}
