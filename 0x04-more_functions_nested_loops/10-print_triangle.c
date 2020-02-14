15d1322923fe3e03bc59f0363b20da43c8a56239
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-14 22:35:45 +0000

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ * @size: Variable
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int l;
+	int colum;
+	int ne;
+
+	for (l = 0; l < size; l++)
+	{
+		for (colum = 0; colum < size; colum++)
+		{
+			ne = size - (l + 1);
+			if (colum < ne)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+	if (size == 0)
+	{
+		_putchar('\n');
+	}
+}
