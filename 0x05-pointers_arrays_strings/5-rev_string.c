42151ac0b6c2261741f803b41da373d582bc6199
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-18 18:40:04 +0000

    Reverses a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * rev_string - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int i = 0;
+	int letter;
+	int leng = 0;
+	char letter2;
+	while (s[i] != '\0')
+	{
+		++leng;
+		i++;
+	}
+	for (letter = 0; letter <= leng / 2; letter++)
+	{
+		letter2 = s[letter];
+		s[letter] = s[leng - letter -1];
+		s[leng - letter - 1] = letter2;
+	}
+}
