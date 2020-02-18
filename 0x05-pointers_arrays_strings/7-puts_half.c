68a589c142f612a065f9f2ad676c0029b42a8f28
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-18 20:38:09 +0000

    Puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * puts_half - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int half, max;
+	int count = 0;
+	while (str[count] != '\0')
+	{
+		count++;
+	}
+	max = count;
+	half = max / 2;
+	while (half <= max)
+	{
+		_putchar(str[half]);
+		half++;
+	}
+	_putchar('\n');
+}
