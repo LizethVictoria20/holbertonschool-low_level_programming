7cc1d06180f12ce599d396fcc5136f645974915f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-18 19:21:05 +0000

    Every other charactr

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,11 @@
 void puts2(char *str)
 {
 	int i;
-	for (i = 0; i <= str[i]; i += 2)
+
+	while (str[i] != '\0')
 	{
-		if(str[i] != '\0')
-			_putchar(str[i]);
+		_putchar(str[i]);
+		i += 2;
 	}
 	_putchar('\n');
 }
