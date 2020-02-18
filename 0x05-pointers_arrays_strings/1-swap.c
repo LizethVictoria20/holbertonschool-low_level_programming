266868ad90dc2685e1f2620c185d8c94e6b6073f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-18 04:39:36 +0000

    Replacing values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a: Pointer
+ * @b: Pointer
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
