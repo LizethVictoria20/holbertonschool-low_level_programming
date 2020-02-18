63b42967c16e9dff7438a7b6364d157b40c0d64a
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-18 04:40:38 +0000

    Printing string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _puts - check the code for Holberton School students.
+ * @str: Pointer
+ * Return: Always 0.
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
