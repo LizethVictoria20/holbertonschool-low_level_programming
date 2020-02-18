181bf80921f20d5084448beb9058bc14e406dadf
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-18 20:39:44 +0000

    Puts half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -2,19 +2,21 @@
 #include <stdio.h>
 /**
  * puts_half - check the code for Holberton School students.
- *
+ * @str: Pointer
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
 	int half, max;
 	int count = 0;
+
 	while (str[count] != '\0')
 	{
 		count++;
 	}
 	max = count;
 	half = max / 2;
+
 	while (half <= max)
 	{
 		_putchar(str[half]);
