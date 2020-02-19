077c7365904016abc48fc1e09ff3d1326a525b7a
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-19 18:42:23 +0000

    Copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int letter = 0;
+	int i;
+
+	while (src[letter] != '\0')
+	{
+		letter++;
+	}
+
+	for (i = 0; i < letter && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
+	for ( ; i < letter; i++)
+	{
+		dest[i] = '\0';
+	}
+	return (dest);
+}
