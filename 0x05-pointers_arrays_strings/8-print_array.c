d46c97ee59f346436a0742b81d8c12834c78f756
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-19 01:35:14 +0000

    Print array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * print_array - check the code for Holberton School students.
+ * @a: Pointer
+ * @n: Pointer
  * Return: Always 0.
  */
 void print_array(int *a, int n)
 {
 	int i = 0;
+
 	while (i < n)
 	{
 		if (i < n)
