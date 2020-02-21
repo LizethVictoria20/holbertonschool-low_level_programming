42f121349ecf3d78406996b56eabb6de4294c05f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-21 19:36:36 +0000

    Reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, count;
+
+	for (i = 0, j = n - 1; j >= i; i++, j--)
+	{
+		count = a[i];
+		a[i] = a[j];
+		a[j] = count;
+	}
+	for (i = 0; i < n; i++)
+		;
+}
