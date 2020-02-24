aab6494118cae0570f563d983a3079a4d57cb370
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-24 19:23:41 +0000

    Comparing string and number bytes

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,37 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * _strspn - check the code for Holberton School students.
+ * @s: Pointer
+ * @accept: Pointer
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int count = 0;
+	int bef = 0;
+	int i, j = 0;
+
+	while (s[i] != '\0')
+	{
+		j = 0;
+		bef = count;
+
+		while (accept[j] != '\0')
+		{
+			if (s[i] == accept[j])
+			{
+				count++;
+			}
+			j++;
+		}
+
+		if (count == bef)
+		{
+			break;
+		}
+		i++;
+	}
+	return (count);
+}
