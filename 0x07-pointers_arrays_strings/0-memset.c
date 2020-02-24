ae8dbc694e53156188ccfb435f95a2a4b56a41f2
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-24 18:00:34 +0000

    Function that fills memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,18 +1,17 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ * _memset - prints buffer in hexa
+ * @s: Pointer
+ * @b: Variables
+ * @n: Variable
  * Return: Nothing.
  */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
