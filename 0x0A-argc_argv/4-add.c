dfe9039960c0edfc234ccc141485c8ecd4d3497b
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-28 20:29:42 +0000

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int only_number(*s)
+{
+	while (*s[i] != 0)
+	{
+		if (*s[i] >= 0 && *s[i] >= 9)
+		{
+			i++;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	int i, suma;
+
+	if (argv[i] == 1)
+	{
+		printf("0\n");
+	}
+	
+}
