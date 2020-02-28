4c033c349f262074cded4d514646f1cb90bf565a
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-28 20:24:33 +0000

    Print string

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * @x: Variable neccesary
+ * @argv: Pointer
+ * Return: Nothing.
+ */
+
+int main(int x __attribute__((unused)), char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
