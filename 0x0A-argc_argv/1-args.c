d8087f4d78445de32eb21945d3afedcd85436998
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-28 20:25:13 +0000

    Print numbers of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - Function
+ * @argc: argc
+ * @argv: Pointer
+ * Return: Nothing.
+ */
+
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%i\n", argc - 1);
+	return (0);
+}
