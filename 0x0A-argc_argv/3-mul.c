8f1ce59121198b44d32e04a8f4490d6e0de39238
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-28 20:26:05 +0000

    Multiplies

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Function
+ * @argc: Variable neccesary
+ * @argv: Pointer
+ * Return: Nothing.
+ */
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("ERROR\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
