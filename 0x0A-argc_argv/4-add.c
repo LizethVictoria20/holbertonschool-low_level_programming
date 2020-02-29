d8eefc98ea1de3d4f250799f176e137db804331f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-29 21:40:46 +0000

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -10,15 +10,17 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int suma;
 	for (i = 1; i < argc; i++)
 	{
 		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
-			printf("%s\n", argv[i]);
+			suma += atoi(argv[i]);
 		}
 		else
 		{
 			printf("Error\n");
 		}
 	}
+	printf("%d\n", suma);
 }
