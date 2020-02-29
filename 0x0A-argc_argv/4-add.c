7fb16fdbf7042d1bc61c1fef0fdc3fdfdefb65d7
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-29 21:07:23 +0000

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,29 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int only_number(*s)
+#include <ctype.h>
+/**
+ * main - program to multiply two numbers
+ * @argc: size of argv array and number of command line arguments
+ * @argv: An array of size argc
+ * Return: always successful
+ */
+int main(int argc, char *argv[])
 {
-	while (*s[i] != 0)
+	int i;
+	for (i = 1; i < argc; i++)
 	{
-		if (*s[i] >= 0 && *s[i] >= 9)
+		if (*argv[i] >= '0' && *argv[i] <= '9')
 		{
-			i++;
+			printf("%s\n", argv[i]);
 		}
 		else
 		{
-			return (0);
+			printf("Error\n");
 		}
 	}
-	return (1);
-}
-
-int main(int argc, char **argv)
-{
-	int i, suma;
-
-	if (argv[i] == 1)
-	{
-		printf("0\n");
-	}
-	
 }
