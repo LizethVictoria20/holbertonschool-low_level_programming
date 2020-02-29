0584fd8ad5631c8eefc4bc4246c874a2db8d0ebf
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-29 18:32:25 +0000

    My new name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 /**
  * main - Function
- * @x: Variable neccesary
+ * @argc: Variable neccesary
  * @argv: Pointer
  * Return: Nothing.
  */
 
-int main(int x __attribute__((unused)), char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
