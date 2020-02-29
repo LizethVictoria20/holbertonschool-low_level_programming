e191e62263e0eecd9120b704dcde929e6fa82df4
LizethVictoria20 <lizvictoria754@gmail.com>
2020-02-29 23:08:51 +0000

    Add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 
 /**
+ * isnumber - program to multiply two numbers
+ * @str: An array of size argc
+ * Return: always successful
+ */
+
+int isnumber(char *str)
+{
+	while (*str != '\0')
+	{
+		if (*str < '0' || *str > '9')
+			return (0);
+		str++;
+	}
+	return (1);
+}
+
+/**
  * main - program to multiply two numbers
  * @argc: size of argv array and number of command line arguments
  * @argv: An array of size argc
@@ -16,9 +33,10 @@ int main(int argc, char *argv[])
 	{
 		printf("0\n");
 	}
+
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] >= '1' && *argv[i] <= '9')
+		if (isnumber(argv[i]))
 		{
 			suma += atoi(argv[i]);
 		}
