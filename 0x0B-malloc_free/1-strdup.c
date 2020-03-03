baecda8e924c3eb459f958df6d035ac908756e99
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-03 18:47:43 +0000

    Newly allocated

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * strleng - check the code for Holberton School students.
+ * @str: Variable
+ * Return: Always 0.
+ */
+int strleng(char *str)
+{
+	int i = 0;
+
+	while (str[i] != 0)
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * _strdup - check the code for Holberton School students.
+ * @str: Variable
+ * Return: Always 0.
+ */
+char *_strdup(char *str)
+{
+	int size;
+	char *s;
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	size = strleng(str);
+	s = malloc(sizeof(char) * size);
+	i = 0;
+
+	while (i < size)
+	{
+		s[i] = str[i];
+		i++;
+	}
+	s[i] = '\0';
+	return (s);
+}
