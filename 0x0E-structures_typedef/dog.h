1b6a87e1c9bacec4a56fc2d7bd0f2b1778d57cbc
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-09 21:34:01 +0000

    New type

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - Creating new type
+ * @name: Name
+ * @age: Age
+ * @owner: Owner
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
