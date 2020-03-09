87041351fe42284bd2b8ede3cc09eea9db92e379
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-09 21:44:08 +0000

    Initializing a variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "dog.h"
+/**
+ * init_dog - Prototype about type dog
+ * @d: First member
+ * @name: Second member
+ * @age: Third member
+ * @owner: 3 member
+ * Description: Longer description
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
