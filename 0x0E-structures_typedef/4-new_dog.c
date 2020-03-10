b436a41637949aa62049baa5e81d25d2e33afbbe
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-10 02:14:34 +0000

    Function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,10 +46,7 @@ char *_strcpy(char *dest, char *src)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newd;
-	if (newd == NULL)
-	{
-		return;
-	}
+
 	newd = malloc(sizeof(struct dog));
 	if (newd == NULL)
 		return (NULL);
