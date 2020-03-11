8405120cf68a7465e24b0a0811adb1baa2f2fbe8
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-11 18:23:40 +0000

    Print name

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,7 +4,7 @@
 /**
  * print_name - prints a name
  * @name: name of the person
- *
+ * @f: name second parament second function
  * Return: Nothing.
  */
 void print_name(char *name, void (*f)(char *))
