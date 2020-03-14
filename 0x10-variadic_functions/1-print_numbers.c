79220835d97582613bc3fd82e2b81a64fb09d55e
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-14 15:16:21 +0000

    Print numbers

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - Function that add all numbers.
+ * @separator: Variable
+ * @n: Variable
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+    va_list valist;
+    unsigned int i;
+
+    va_start(valist, n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("%d", va_arg(valist, const unsigned int));
+        if (separator != NULL && (i < (n - 1)))
+        {
+            printf("%s", separator);
+        }
+    }
+    printf("\n");
+}
