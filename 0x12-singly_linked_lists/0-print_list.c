85374003c3f332fe143dd686a752fd7eff645b8f
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-19 19:43:26 +0000

    Print all elements

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,28 +1,28 @@
 #include "lists.h"
-
 /**
- * list_s - singly linked list
- * @h: string - (malloc'ed string)
+ * print_list - prints all the elements of a list_t list
+ * @h: Head list
+ * Return: number of elements
  */
 
 size_t print_list(const list_t *h)
 {
-    size_t size;
+	size_t size;
 
-    size = 0;
+	size = 0;
 
-    while (h != NULL)
-    {
-        size++;
-        if (h->str == NULL)
-        {
-            printf("[%d] (nil)\n", 0);
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
-        h = h->next;
-    }
-    return (size);
+	while (h != NULL)
+	{
+		size++;
+		if (h->str == NULL)
+		{
+			printf("[%d] (nil)\n", 0);
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+	}
+	return (size);
 }
