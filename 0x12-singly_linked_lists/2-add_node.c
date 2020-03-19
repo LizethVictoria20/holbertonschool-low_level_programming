5baa756c721f850899b574c84ae3d3164fcc5d5e
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-19 21:28:25 +0000

    Add new nodo start

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+/**
+ * *add_node - function to add node to start
+ * @head: pointer to pointer of list_t struct type
+ * @str: pointer
+ * Return: Always Successful
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	int length;
+	char *string;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	length = 0;
+
+	while (str[length] != '\0')
+	{
+		string = strdup(str);
+		length++;
+	}
+
+	new->len = length;
+	new->str = string;
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
