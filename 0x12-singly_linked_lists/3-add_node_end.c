208890d69aade6d827f70a6c8215aa0c0107a4bc
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-20 00:58:29 +0000

    Add node to end

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,59 @@
+#include "lists.h"
+
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: Pointer
+ * Return: Always 0.
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+	{
+		len++;
+	}
+	return (len);
+}
+
+/**
+ * add_node_end - function to add node to end
+ * @head: pointer to pointer of list_t struct type
+ * @str: pointer
+ * Return: Always Successful
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new, *tmp;
+	int length;
+
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+
+	new->len = length;
+	new->str = strdup(str);
+
+	if (*head == NULL)
+	{
+		*head = new;
+		return (*head);
+	}
+	tmp = *head;
+
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next;
+	}
+	tmp->next = new;
+
+	return (*head);
+}
