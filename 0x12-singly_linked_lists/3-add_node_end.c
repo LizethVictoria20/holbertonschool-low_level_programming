8946816c8a834a16c46cac812b206765ccc9a831
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-20 01:04:18 +0000

    Add node to end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -27,16 +27,14 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *tmp;
 	int length;
 
-
-	while (str[length] != '\0')
-	{
-		length++;
-	}
-
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 	{
 		return (NULL);
+	}
+	while (str[length] != '\0')
+	{
+		length++;
 	}
 
 	new->len = length;
@@ -54,6 +52,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		tmp = tmp->next;
 	}
 	tmp->next = new;
+	new->next = NULL;
 
 	return (*head);
 }
