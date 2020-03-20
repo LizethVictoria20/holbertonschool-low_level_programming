5053dadafa024a8368ba2a366bd09b1a5ad50738
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-20 01:57:50 +0000

    Print first main

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,12 @@
 
 void __attribute__((constructor)) phrase(void);
 /**
- * primary - print before
+ * phrase - print before
  * Return: void
  */
 
 void phrase(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\nI bore my house upon ");
+	printf("my back!\n")
 }
