c696c997e64bb68f7829febd46c62e6bf7627d9e
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-23 13:04:03 +0000

    Leng list

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Function that prints all elements from the list.
+ * @h: Elements
+ * Return: Always 0.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	size_t size;
+
+	size = 0;
+
+	while (h != NULL)
+	{
+		size++;
+		h = h->next;
+	}
+	return (size);
+}
