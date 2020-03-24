f1460a16af82de0f9a85bbd93939834ab09094da
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-24 13:49:12 +0000

    Delete head node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * pop_listint - function to delete heade node
+ * @head: pointer to pointer of listint_t struct type
+ * Return: Always Successful
+ */
+
+int pop_listint(listint_t **head)
+{
+	listint_t *new;
+	int n;
+
+	new = *head;
+	n = 0;
+
+	if (*head != NULL)
+	{
+		return (n);
+	}
+	n = head->n;
+	*head = new->next;
+	n = new->n;
+
+	return (n);
+}
