5cab4a011b2f01bc6e6ced5848c0387827b56740
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-26 20:53:41 +0000

    Decimal to binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,11 @@
 
 void print_binary(unsigned long int n)
 {
-    if (n < 2)
-   {
-       _putchar(n + '0');
-       return;
-   } 
-   print_binary(n >> 1);
-   _putchar((n & 1) + '0');
+	if (n < 2)
+	{
+		_putchar(n + '0');
+		return;
+	}
+	print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
