4599f46ea7627d9a6c80e7b39f46c2929f7f01ff
LizethVictoria20 <lizvictoria754@gmail.com>
2020-03-26 20:41:07 +0000

    Decimal to binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_binary - Function that converts a binary number to decimal.
+ * @n: Elements
+ * Return: Always 0.
+ */
+
+void print_binary(unsigned long int n)
+{
+    if (n > 1)
+   {
+    print_binary(n >> 2);
+   } 
+   _putchar((n & 1) + '0');
+}
