1f9b48542161f8eb10195c2ce49bcf89e7478514
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-01 22:35:27 +0000

    Copie

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "holberton.h"
+
+/**
+ * close_file1 - check the code for Holberton School students.
+ * @text: file
+ * Return: Always 0.
+ */
+
+void close_file1(int text)
+{
+	int close_file = close(text);
+
+	if (close_file == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", text);
+		exit(100);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ * @ac: file
+ * @argv: Arguments
+ * Return: Always 0.
+ */
+
+int main(int ac, char **argv)
+{
+	int read_file, write_file;
+	int from_file, to_file;
+	int buff[1024];
+
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	from_file = open(argv[1], O_RDONLY);
+	if (from_file == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s", argv[1]);
+		exit(98);
+	}
+	to_file = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
+
+	if (to_file == -1)
+	{
+		dprintf(2, "Error: Can't write to %s", argv[2]);
+		exit(99);
+	}
+	for (read_file = 1024; read_file == 1024;)
+	{
+		read_file = read(from_file, buff, 1024);
+		if (read_file == -1)
+		{
+			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			exit(98);
+		}
+		write_file = write(to_file, buff, read_file);
+		if (write_file == -1)
+		{
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close_file1(to_file);
+	close_file1(from_file);
+	return (0);
+}
