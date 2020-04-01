43b961f1980e9f006be6ee333e4d7991b7c969e9
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-01 17:49:26 +0000

    Read file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,16 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-
 	space = malloc(sizeof(char) * letters);
-
 	if (space == NULL)
 	{
 		return (-1);
 	}
-
 	fd = open(filename, O_RDONLY);
-
 	if (fd == -1)
 	{
 		return (0);
@@ -37,9 +33,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (-1);
 	}
-
 	fdwrite = write(STDOUT_FILENO, space, fdread);
-
 	if (fdwrite == -1)
 	{
 		return (-1);
@@ -51,6 +45,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (-1);
 	}
-
 	return (fdread);
 }
