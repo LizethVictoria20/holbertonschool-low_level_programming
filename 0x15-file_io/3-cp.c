319d966eadad8c76c0c94324173c581bd706b973
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-01 22:46:01 +0000

    Copie

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,11 +8,13 @@
 
 void close_file1(int text)
 {
-	int close_file = close(text);
+	int close_file;
+
+	close_file = close(text);
 
 	if (close_file == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", text);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", text);
 		exit(100);
 	}
 }
@@ -32,20 +34,20 @@ int main(int ac, char **argv)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	from_file = open(argv[1], O_RDONLY);
 	if (from_file == -1)
 	{
-		dprintf(2, "Error: Can't read from file %s", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s", argv[1]);
 		exit(98);
 	}
 	to_file = open(argv[2], O_CREAT | O_TRUNC | O_WRONLY, 0664);
 
 	if (to_file == -1)
 	{
-		dprintf(2, "Error: Can't write to %s", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s", argv[2]);
 		exit(99);
 	}
 	for (read_file = 1024; read_file == 1024;)
@@ -53,13 +55,13 @@ int main(int ac, char **argv)
 		read_file = read(from_file, buff, 1024);
 		if (read_file == -1)
 		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 		write_file = write(to_file, buff, read_file);
 		if (write_file == -1)
 		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
 		}
 	}
