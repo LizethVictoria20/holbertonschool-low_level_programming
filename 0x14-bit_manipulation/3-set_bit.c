1b5508f10d86274aad8d8ae0aa0d8edaaa0cecc1
LizethVictoria20 <lizvictoria754@gmail.com>
2020-04-15 04:32:37 +0000

    Set bit

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,20 +1,18 @@
 #include "holberton.h"
-#include <stdio.h>
 
 /**
- * set_bit - Function that sets the value of a bit
- * @n: Elements
- * @index: Ele
- * Return: Always 0.
+ * set_bit -  function to set value of bit at given index
+ * @n: binary number given
+ * @index:  index  given
+ * Return: 1 if successful or -1 if failed
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	if (index > sizeof(unsigned long int) * 8)
+	if (index  < sizeof(n) *  8)
 	{
-		return (-1);
+		*n =  *n | (1 << index);
+		return (1);
 	}
-
-	*n |= 1 << index;
-	return (1);
+	return (-1);
 }
