42aec9d4e2a6fb90b148e14d4e7a09b7bc81a1cb
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-26 18:26:11 +0000

    Update

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,7 +19,7 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+		printf("Value checked array[%d] = [%d]\n", (int) i, array[i]);
 
 		if (array[i] == value)
 		{
