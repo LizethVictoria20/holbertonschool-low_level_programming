aae44d7e9360f4dea243abf7316385ab49fb0bac
LizethVictoria20 <lizvictoria754@gmail.com>
2020-08-26 21:01:23 +0000

    Jump search

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,35 @@
+#include "search_algos.h"
+/**
+ * jump_search - Function linear search
+ * @array: Array is a pointer to the first element
+ * @size: Size is the number of elements in array
+ * @value: Value is the value to search for
+ * Return: Always EXIT_SUCCESS
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t start = 0, end = sqrt(size), i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	while (end < size && array[end] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n",
+			(int) start, array[start]);
+		start = end;
+		end += sqrt(size);
+	}
+	printf("Value checked array[%d] = [%d]\n", (int) start, array[start]);
+	printf("Value found between indexes [%d] and [%d]\n",
+		(int) start, (int) end);
+
+	for (i = start; i <= end && i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", (int) i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
+}
